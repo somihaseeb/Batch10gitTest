56a084bb7451368746404e6a702b553c462372a7
somihaseeb <somihaseeb@gmail.com>
2021-08-11 19:36:12 -0400

    first commit of project

---
+++ src/gitfinalclass/gitclass03.java
@@ -0,0 +1,12 @@
+package gitfinalclass;
+
+public class gitclass03 {
+
+	public static void main(String[] args) {
+		// new class for git
+		
+		System.out.println("This is the first commit file");
+
+	}
+
+}
